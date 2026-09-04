b82687d14b762f74f156b625e5db338318e53c86
Bobabybeater3547 <shi.guanqi.eee@gmail.com>
2026-09-04 21:25:20 +0900

    Delete boot.js

--- boot.js
+++
@@ -1,3 +0,0 @@
-// Compatibility capture must run before any app resources. No private query transport.
-window.__ledgerFragment = location.hash.slice(1);
-if (location.hash || location.search) history.replaceState(null, '', location.pathname);
