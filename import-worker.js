50d216891432e84d23e5777ad20eae3c1831eaf7
Bobabybeater3547 <shi.guanqi.eee@gmail.com>
2026-09-04 21:25:37 +0900

    Delete import-worker.js

--- import-worker.js
+++
@@ -1,2 +0,0 @@
-import {parseImport} from './core.js';
-self.onmessage=async({data})=>{try{self.postMessage({result:await parseImport(data.text,data.ledgerId)});}catch(e){self.postMessage({error:e.message});}};
